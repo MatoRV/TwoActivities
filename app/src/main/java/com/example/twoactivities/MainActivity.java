50cb4d069b096ce0491be84d2c8628d609e2de12
Víctor Mato Ramos <victormator5@gmail.com>
2023-10-10 23:18:36 +0200

    Commit inicial

---
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.twoactivities;
+
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Esta constante utiliza el nombre de la clase misma como etiqueta
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void activarSegundaActividad(View view) {
+        Log.d(LOG_TAG, "Botón clicado");
+
+    }
+}
