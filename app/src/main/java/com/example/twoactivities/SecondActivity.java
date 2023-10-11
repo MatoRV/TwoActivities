9b7dec64fcf92732f1eb42e65dea946bc2194f9b
Víctor Mato Ramos <victormator5@gmail.com>
2023-10-11 17:25:47 +0200

    Proyecto finalizado

--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public static final String RESPUESTA_EXTRA = "com.example.android.twoactivities.extra.REPLY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,21 @@ public class SecondActivity extends AppCompatActivity {
 
         // Introduce el texto del TextView
         tvTextMessage.setText(mensaje);
+    }
+
+    public void returnReply(View view) {
+        EditText mRespuesta = findViewById(R.id.editText_second);
+
+        String reply = mRespuesta.getText().toString();
+
+        Intent replyIntent = new Intent();
+
+        replyIntent.putExtra(RESPUESTA_EXTRA,reply);
+
+        // Poniendo como resultado RESULT_OK indica que la respuesta fue un éxito.
+        setResult(RESULT_OK, replyIntent);
+
+        // Llamando a finish cierra la actividad y vuelve a MainActivity.
+        finish();
     }
 }
